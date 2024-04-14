ac2ed712171a4b7b4d6fd090a2b8ed052a32dfdd
DevHN504 <155341765+DevHN504@users.noreply.github.com>
2024-04-13 18:16:32 -0600

    Add project files.

---
+++ SeekbarApp/SeekbarApp.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Android.Widget;
+
+namespace SeekbarApp.Droid
+{
+    [Activity(Label = "SeekbarApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            // Set the content view for the current activity
+            SetContentView(Resource.Layout.seekbar_layout);
+
+            // To find the views
+            SeekBar seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            TextView textView = FindViewById<TextView>(Resource.Id.textView);
+
+            // Set a listener
+            seekBar.ProgressChanged += (sender, e) =>
+            {
+                textView.Text = $"{e.Progress}%";
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
